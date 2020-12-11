be5bf18f39401c407ea8dabdf6d57a293c60eac6
VictorJaramilloC <victorstrike93@gmail.com>
2020-12-11 17:48:05 -0600

    menu corregido

--- My-App/resources/views/home.blade.php
+++ My-App/resources/views/home.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.adminlte')
 
 @section('content')
-    <center><h1 class="display-1">Bienvenido {{Auth::user()->name}}</h1></center>
+    <center><h1 class="display-1">Bienvenido {{Auth::user()->name}}
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis, facere temporibus magnam sed quibusdam explicabo iure ab id voluptatibus eaque nobis nesciunt molestiae illo exercitationem quod obcaecati, hic labore libero.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam atque perspiciatis ab ea eum doloribus ipsum, omnis, optio provident sapiente est, molestiae quidem commodi. Facilis blanditiis dolorum quidem perspiciatis officia!
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus dolorum modi facere, quia est nisi ducimus! Accusamus repellendus atque delectus culpa explicabo adipisci aut, rem hic, vel ut maxime maiores!
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam provident quod ipsam totam qui dolorum quia fuga officia alias hic, consequatur, velit error in debitis repudiandae necessitatibus eaque officiis porro!
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, impedit! Cumque iure nihil porro blanditiis perspiciatis? Ipsa iusto pariatur delectus, quaerat aliquid beatae enim ab asperiores eum, libero tempora? Unde.
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero ratione vitae sunt neque debitis distinctio consequatur et minima asperiores optio? Officia est ea aliquid, debitis quaerat nulla maiores perspiciatis incidunt!
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit praesentium enim distinctio a. Quos, esse molestiae at, ipsum excepturi omnis nisi, velit rerum maxime consequatur vel. Labore necessitatibus exercitationem deserunt.
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit doloremque fugit quas accusamus culpa temporibus distinctio voluptatum aliquid minus! Iusto ullam rem possimus laborum tempora ducimus natus? Nulla, aspernatur possimus.
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, laudantium! Perspiciatis iste voluptas quae magni sapiente cupiditate, velit deserunt cum voluptatibus incidunt fuga aperiam itaque asperiores sint quibusdam dolore tempore.
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro autem veniam veritatis officia qui facere, eum harum odit iusto illum fugit, perspiciatis ratione ex rerum recusandae error similique consequatur laboriosam.
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum velit itaque dolor, ex dolorum exercitationem sit, provident perferendis autem quaerat incidunt blanditiis cumque ad voluptate odit debitis rem aliquid nostrum.
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta facere voluptate earum? Dolorem nulla laboriosam vel doloribus consequatur omnis officia distinctio quo deleniti maiores! Nihil quibusdam laborum dolorum incidunt quod!
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores deserunt, optio autem quia dignissimos ea eligendi itaque sapiente reiciendis nulla obcaecati eveniet aspernatur non modi repellendus exercitationem ut maiores excepturi.
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem esse debitis temporibus saepe in ipsam voluptatum delectus, voluptas consequatur tempora enim veniam, accusamus vero earum numquam, quasi modi sint ducimus.</h1></center>
 @endsection
--- My-App/resources/views/layouts/adminlte.blade.php
+++ My-App/resources/views/layouts/adminlte.blade.php
@@ -6,11 +6,11 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-
   <title>SICON | @yield('title')</title>
-  
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!--CSS personalizadas de SICON-->
   <link rel="stylesheet" href="../css/content_header.css">
   <link rel="stylesheet" href="../css/background.css">
   <!-- Font Awesome Icons -->
@@ -20,13 +20,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
-<body class="hold-transition sidebar-mini">
+<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed sidebar-collapse ">
 <div class="wrapper">
 
   <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand navbar-white navbar-dark" style="background: rgb(146, 25, 40)">
+  
+  <nav class="main-header text-sm navbar navbar-expand navbar-white navbar-dark" style="background: rgb(146, 25, 40) ">
     <!-- Left navbar links -->
-    <ul class="navbar-nav">
+    <ul class="navbar-nav ">
       <li class="nav-item">
         <a class="nav-link" data-widget="pushmenu" href="#" 
         data-no-transition-after-reload="false"
@@ -52,167 +53,174 @@ scratch. This page gets rid of all links and provides the needed markup only.
       </li>
       @endif
       <li class="nav-item">
-        <a href="{{ route('logout') }}" class="nav-link">
+        <a href="{{ url('/logout') }}" class="nav-link">
           <i class="fas fa-power-off"></i> CERRAR SESIÓN
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
+          <i class="fas fa-th-large"></i>
         </a>
       </li>
       
     </ul>
   </nav>
   <!-- /.navbar -->
+  <div class="menu_completo">
+<!-- Main Sidebar Container -->
 
-  <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-light-primary elevation-4" >
-    <!-- Brand Logo -->
-    <a href="{{route('home')}}" class="brand-link">
-      <img src="../dist/img/salud.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-           style="opacity: .8">
-      <span class="brand-text font-weight-light">SICON</span>
-    </a>
+<aside class="main-sidebar sidebar-light-secondary elevation-4" >
+  <!-- Brand Logo -->
+  <a href="{{route('home')}}" class="brand-link">
+    <img src="../dist/img/salud.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+         style="opacity: .8">
+    <span class="brand-text font-weight-light">SICON</span>
+  </a>
 
-    <!-- Sidebar -->
-    <div class="sidebar" data-accordion="false">
-      <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="../dist/img/avatar_SS2.jpg" class="img-circle elevation-2" alt="User Image">
-        </div>
-        <div class="info">
-          <a href="{{route('home')}}" class="d-block">{{Auth::user()->name}}</a>
-        </div>
+  <!-- Sidebar -->
+  <div class="sidebar" data-accordion="true">
+    <!-- Sidebar user panel (optional) -->
+    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+      <div class="image">
+        <img src="../dist/img/avatar_SS2.jpg" class="img-circle elevation-2" alt="User Image">
       </div>
-
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-               @if (Auth::user()->id_rol==0 || Auth::user()->id_rol==1 || Auth::user()->id_rol==2 || Auth::user()->id_rol==3 || Auth::user()->id_rol==4)
-               <li class="nav-item">
-                  @if (Auth::user()->id_rol==0)
-                <a href="{{route('DDSCH.capDDSCH')}}" class="nav-link">
-                  @endif
-                  @if (Auth::user()->id_rol==1)
-                  <a href="{{route('DDSCH.autorizaDDSCH')}}" class="nav-link">
-                    @endif
-                  @if (Auth::user()->id_rol==2)
-                <a href="{{route('DSPO.autorizaDSPO')}}" class="nav-link">
-                  @endif
-                  @if (Auth::user()->id_rol==3)
-                <a href="{{route('DSPO.capDSPO')}}" class="nav-link">
-                  @endif
-                  @if (Auth::user()->id_rol==4)
-                <a href="{{route('DSPO.autorizaDSPO')}}" class="nav-link">
-                  @endif
-                  <i class="fas fa-file-signature"></i>
-                  <p>
-                    Bandeja
-                  </p>
-                </a>
-              </li>
-              @endif
-              @if (Auth::user()->id_rol==0 || Auth::user()->id_rol==1 || Auth::user()->id_rol==4)
-              <li class="nav-item">
-                <a href="{{route('General.filtroDescargar')}}" class="nav-link">
-                  <i class="fas fa-file-download"></i>
-                  <p>
-                   Descarga de Documentos
-                  </p>
-                </a>
-              </li>
-              @endif
-              @if (Auth::user()->id_rol==0 || Auth::user()->id_rol==1 || Auth::user()->id_rol==2 || Auth::user()->id_rol==4)
-              <li class="nav-item">
-                <a href="{{route('General.generarReporte')}}" class="nav-link">
-                  <i class="fas fa-book"></i>
-                  <p>
-                    Generar Reporte
-                  </p>
-                </a>
-              </li>
-              @endif
-              @if (Auth::user()->id_rol==3)
-              <li class="nav-item">
-                <a href="{{route('DSPO.generarReportePC')}}" class="nav-link">
-                  <i class="fas fa-book"></i>
-                  <p>
-                    Reporte Profesional
-                  </p>
-                </a>
-              </li>
-              @endif
-              @if (Auth::user()->id_rol==0 || Auth::user()->id_rol==1 || Auth::user()->id_rol==2 || Auth::user()->id_rol==3 || Auth::user()->id_rol==4 || Auth::user()->id_rol==5)
-              <li class="nav-item">
-                <a href="{{route('General.consultaEstado')}}" class="nav-link">
-                  <i class="fas fa-search"></i>
-                  <p>
-                    Consulta
-                  </p>
-                </a>
-              </li>
-              @endif
-              @if (Auth::user()->id_rol==0 || Auth::user()->id_rol==1)
-          <li class="nav-item has-treeview menu-open">
-              <i class="fas fa-file-upload"></i>
-              <p>
-                Guardar
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="{{route('General.guardarVista')}}" class="nav-link active">
-                  <i class="fas fa-arrow-circle-up"></i>
-                  <p>Guardar Documentos</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="{{route('DDSCH.guardarVistaEventuales')}}" class="nav-link">
-                  <i class="fas fa-arrow-circle-up"></i>
-                  <p>Guardar Documentos Eventuales</p>
-                </a>
-              </li>
-            </ul>
-          </li>
-          @endif
-          @if (Auth::user()->id_rol==2 || Auth::user()->id_rol==3)
-          <li class="nav-item">
-            <a href="{{route('General.guardarVista')}}" class="nav-link">
-              <i class="fas fa-file-upload"></i>
-              <p>Guardar Documentos</p>    
-            </a>
-          </li>
-          @endif
-          @if (Auth::user()->id_rol==0 || Auth::user()->id_rol==1)
-          <li class="nav-item">
-            <a href="{{route('DDSCH.qrtxt')}}" class="nav-link">
-              <i class="fas fa-qrcode"></i>
-              <p>Guardar txt</p>    
-            </a>
-          </li>
-          @endif
-          @if (Auth::user()->id_rol==0 || Auth::user()->id_rol==1)
-          <li class="nav-item">
-            <a href="{{route('DDSCH.actualizarFecha')}}" class="nav-link">
-              <i class="fas fa-calendar-alt"></i>
-              <p>Actualizar Fecha</p>
-            </a>
-          </li>
-          @endif
-          @if (Auth::user()->id_rol==2)
-          <li class="nav-item">
-            <a href="{{route('DSPO.correosUR')}}" class="nav-link">
-              <i class="fas fa-at"></i>
-              <p>Correos</p>
-            </a>
-          </li>
-          @endif
-        </ul>
-      </nav>
-      <!-- /.sidebar-menu -->
+      <div class="info">
+        <a href="{{route('home')}}" class="d-block">{{Auth::user()->name}}</a>
+      </div>
     </div>
-    <!-- /.sidebar -->
-  </aside>
+
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <!-- Add icons to the links using the .nav-icon class
+             with font-awesome or any other icon font library -->
+             @if (Auth::user()->id_rol==0 || Auth::user()->id_rol==1 || Auth::user()->id_rol==2 || Auth::user()->id_rol==3 || Auth::user()->id_rol==4)
+             <li class="nav-item">
+                @if (Auth::user()->id_rol==0)
+              <a href="{{route('DDSCH.capDDSCH')}}" class="nav-link">
+                @endif
+                @if (Auth::user()->id_rol==1)
+                <a href="{{route('DDSCH.autorizaDDSCH')}}" class="nav-link">
+                  @endif
+                @if (Auth::user()->id_rol==2)
+              <a href="{{route('DSPO.autorizaDSPO')}}" class="nav-link">
+                @endif
+                @if (Auth::user()->id_rol==3)
+              <a href="{{route('DSPO.capDSPO')}}" class="nav-link">
+                @endif
+                @if (Auth::user()->id_rol==4)
+              <a href="{{route('DSPO.autorizaDSPO')}}" class="nav-link">
+                @endif
+                <i class="fas fa-file-signature"></i>
+                <p>
+                  Bandeja
+                </p>
+              </a>
+            </li>
+            @endif
+            @if (Auth::user()->id_rol==0 || Auth::user()->id_rol==1 || Auth::user()->id_rol==4)
+            <li class="nav-item">
+              <a href="{{route('General.filtroDescargar')}}" class="nav-link">
+                <i class="fas fa-file-download"></i>
+                <p>
+                 Descarga de Documentos
+                </p>
+              </a>
+            </li>
+            @endif
+            @if (Auth::user()->id_rol==0 || Auth::user()->id_rol==1 || Auth::user()->id_rol==2 || Auth::user()->id_rol==4)
+            <li class="nav-item">
+              <a href="{{route('General.generarReporte')}}" class="nav-link">
+                <i class="fas fa-book"></i>
+                <p>
+                  Generar Reporte
+                </p>
+              </a>
+            </li>
+            @endif
+            @if (Auth::user()->id_rol==3)
+            <li class="nav-item">
+              <a href="{{route('DSPO.generarReportePC')}}" class="nav-link">
+                <i class="fas fa-book"></i>
+                <p>
+                  Reporte Profesional
+                </p>
+              </a>
+            </li>
+            @endif
+            @if (Auth::user()->id_rol==0 || Auth::user()->id_rol==1 || Auth::user()->id_rol==2 || Auth::user()->id_rol==3 || Auth::user()->id_rol==4 || Auth::user()->id_rol==5)
+            <li class="nav-item">
+              <a href="{{route('General.consultaEstado')}}" class="nav-link">
+                <i class="fas fa-search"></i>
+                <p>
+                  Consulta
+                </p>
+              </a>
+            </li>
+            @endif
+            @if (Auth::user()->id_rol==0 || Auth::user()->id_rol==1)
+        <li class="nav-item has-treeview menu-open">
+          <a href="" class="nav-link">
+            <i class="fas fa-file-upload"></i>
+            <p>Guardar</p>
+              <i class="right fas fa-angle-left"></i>
+            </p>
+          </a>
+          <ul class="nav nav-treeview">
+            <li class="nav-item">
+              <a href="{{route('General.guardarVista')}}" class="nav-link">
+                <i class="fas fa-arrow-circle-up"></i>
+                <p>Guardar Documentos</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="{{route('DDSCH.guardarVistaEventuales')}}" class="nav-link">
+                <i class="fas fa-arrow-circle-up"></i>
+                <p>Documentos Eventuales</p>
+              </a>
+            </li>
+          </ul>
+        </li>
+        @endif
+        @if (Auth::user()->id_rol==2 || Auth::user()->id_rol==3)
+        <li class="nav-item">
+          <a href="{{route('General.guardarVista')}}" class="nav-link">
+            <i class="fas fa-file-upload"></i>
+            <p>Guardar Documentos</p>    
+          </a>
+        </li>
+        @endif
+        @if (Auth::user()->id_rol==0 || Auth::user()->id_rol==1)
+        <li class="nav-item">
+          <a href="{{route('DDSCH.qrtxt')}}" class="nav-link">
+            <i class="fas fa-qrcode"></i>
+            <p>Guardar txt</p>    
+          </a>
+        </li>
+        @endif
+        @if (Auth::user()->id_rol==0 || Auth::user()->id_rol==1)
+        <li class="nav-item">
+          <a href="{{route('DDSCH.actualizarFecha')}}" class="nav-link">
+            <i class="fas fa-calendar-alt"></i>
+            <p>Actualizar Fecha</p>
+          </a>
+        </li>
+        @endif
+        @if (Auth::user()->id_rol==2)
+        <li class="nav-item">
+          <a href="{{route('DSPO.correosUR')}}" class="nav-link">
+            <i class="fas fa-at"></i>
+            <p>Correos</p>
+          </a>
+        </li>
+        @endif
+      </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
+  </div>
+  <!-- /.sidebar -->
+</aside>
+</div>
 
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
@@ -238,7 +246,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
 
   <!-- Main Footer -->
-  <footer class="main-footer">
+  <footer class="main-footer text-sm">
     <!-- To the right -->
     <div class="float-right d-none d-sm-inline">
       SICON
@@ -246,6 +254,11 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Default to the left -->
     <strong>Copyright &copy; 2019-2020 <a href="https://www.gob.mx/salud">Secretaria de Salud</a>.</strong> Todos los derechos reservados.
   </footer>
+
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+      <!-- Control sidebar content goes here -->
+    </aside>
 </div>
 <!-- ./wrapper -->
 
@@ -257,5 +270,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
 <script src="../dist/js/adminlte.min.js"></script>
+
 </body>
 </html>
