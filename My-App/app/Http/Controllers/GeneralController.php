c047bb3b09d9e9b4e9669399b73a0a7c01298455
AlexisH1project <projects.h1mx@gmail.com>
2020-12-18 16:45:35 -0600

    AUTOCOMPLETAR RFC (primra paarte)

--- My-App/app/Http/Controllers/GeneralController.php
+++ My-App/app/Http/Controllers/GeneralController.php
@@ -8,7 +8,11 @@ use Illuminate\Support\Facades\DB;
 class GeneralController extends Controller
 {
   
+    public function index()
+    {
+        return view('General.guardarVista');
 
+    }
   
     public function consultaEstado(){
 
@@ -25,23 +29,63 @@ class GeneralController extends Controller
         //$Fomope = DB::table('fomope')->get();
         return view('General.generarReporte');
     }
-    public function guardarVista(){
-
-        //$Fomope = DB::table('fomope')->get();
-        return view('General.guardarVista');
-    }
-
-    public function resultados_curp(Request $request){
+    public function resultados_rfc(Request $request){
         $term = $request->get('term');
-        $resultado = DB::table('ct_empleados')->select('*')->where('curp', 'LIKE', '%'.$term.'%')->get();
+        $resultado = DB::table('ct_empleados')->select('*')->where('rfc', 'LIKE', '%'.$term.'%')->get();
+        $contador = 0;
         $data = [];
         foreach ($resultado as $registro) {
-            # code...
-            $data[] = [
-                'curp' => $registro->curp
-            ];
+            $data[$contador] = $registro->rfc;
+            $contador++;
         }
         return $data;
+    }
+
+    public function resultadosC_rfc(Request $request){
+
+        $term = $request->get('term2');
+        $empleado = DB::table('ct_empleados')
+        ->where('rfc',$term)
+        ->get();
+        // foreach ($empleado as $e1) {
+        //     $value[0]= array( 
+        //         "apellido1"=> $e1->apellido_1,
+        //         "apellido2"=> $e1->apellido_2,
+        //         "nombre"=> $e1->nombre,
+        //         "curp"=> $e1->curp
+        //     );
+        // }
+        //var_dump($value);
+
+        $value[0]= array( 
+            "apellido1"=> "SS",
+            "apellido2"=> "SDSDS",
+            "nombre"=> "SASA",
+            "curp"=> "ASS"
+        );
+
+        // $resFomope = DB::table('fomope')
+        // ->where('rfc',$empleado->rfc)
+        // ->get();
+       
+        // $value[1]= 0;
+
+       
+        // $value[1]= 0;
+        // $i = 1;
+        // foreach ($resFomope as $row) {
+
+        //     $value[$i] = array( 
+        //         "id"-> $row->id_movimiento,
+        //         "codigo"-> $row->codigoMovimiento,
+        //         "fecha"-> $row->vigenciaDel,
+        //         "anio"-> $row->anio,
+        //         "qna"-> $row->qnaDeAfectacion
+        //     );
+        //     $i++;
+        // }
+        //return $value;
+        echo json_encode($value);
     }
 
   
--- My-App/resources/views/General/guardarVista.blade.php
+++ My-App/resources/views/General/guardarVista.blade.php
@@ -3,50 +3,86 @@
 
 @section('content')
 <!-- <link rel = "stylesheet" href= "{{ asset('css/jquery/jquery-ui.min.css') }}"> -->
-
+<body>
+    <form>
     <!-- <form  enctype="multipart/form-data" id="formDatos" name="captura1" action="" method="POST">  -->
                         <div class="form-row">
                             <div class="col">
-                                <div class="form-group col-md-12">
-                                    <label class="plantilla-label" for="elRfc">*CURP:</label>
+                                <div class="form-group col-md-4">
+                                    <label class="plantilla-label" for="elRfc">*RFC:</label>
                                     
-                                    <input type="text" class="" id="curp" name="curp" placeholder="CURP" placeholder="Ingresa curp" maxlength="13"  required>
+                                    <input type="text" class="form-control" id="rfc" name="rfc" placeholder="RFC" placeholder="Ingresa rfc" maxlength="13" required>
+                                    
+                                    <div id="countryList">
+                                    </div>
                                 </div>
                             </div>
                         </div>
                         <input type="button" class="" value="Aceptar" name="botonAccion">              
-	<!-- </form> -->
+	</form>
 
-    <script>
- 
-        alert("sss");
-
-        var opciones = ['carro', 'moto', 'bici'];
+    <script type="text/javascript">
         $(document).ready(function () { 
-           $('#curp').autocomplete({
+            $('#rfc').autocomplete({
                 source: function(request, response){
                     $.ajax({
-                        url:"{{ route('serch.guadarDoc')}}",
+                        url:"{{ route('Serch.rfc')}}",
                         dataType:'json',
                         data:{
                             term: request.term
                         },
                         success: function(data){
                             response(data)
-                            
                         }
                     });
-                }
+                },select: function (event, ui){
+							$(this).val(ui.item.label);
+							var buscarid = ui.item.value;
+							console.log(buscarid);
+							//alert(buscarid);
+							$.ajax({
+								url: "{{ route('Serch.Crfc')}}",
+								dataType: 'json',
+								data: {
+									term2: buscarid.term2									
+								},
+								success: function(data){
+									var infEmpleado = eval(data);
+									console.log(data);
+									console.log(infEmpleado[0].apellido1);
+								    //   console.log(infEmpleado.length);
+
+                                      //document.getElementById("rfc").value = infEmpleado[1] ;
+
+
+								// 	document.getElementById("apellido1").value = infEmpleado[0].apellido1 ;
+								// 	document.getElementById("apellido2").value = infEmpleado[0].apellido2 ;
+								// 	document.getElementById("nombre").value = infEmpleado[0].nombre ;
+									
+								//   for(var i=1; i < infEmpleado.length; i++){ 
+								//         console.log(infEmpleado[i]);
+								// 	    if(infEmpleado[i].id != null){
+
+								//         var miSelect2 = document.getElementById("movFecha");
+								// 	    var aTag = document.createElement('option');
+								// 	    aTag.setAttribute('value',infEmpleado[i].id);
+								// 	    aTag.innerHTML = "( Codigo: "+infEmpleado[i].codigo+" ) ( Fecha: "+infEmpleado[i].fecha+" ) (Qna: "+infEmpleado[i].qna+") (Año: "+infEmpleado[i].anio+" )";
+								// 	    miSelect2.appendChild(aTag);
+								// 		}
+								// 	}
+
+
+								}
+							});
+							return false;
+						}
             });
         });
-        // $(document).ready(function () { 
-                
-        //         $('#curp').autocomplete({
-        //         source: opciones
-                
-        //         });
-        //     });
-    </script>
+
+
+        </script>
+
+</body>
 @endsection
 
 
